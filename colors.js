4a97071621f6053aeaa5f4b8089a32d87edb27c0
Paramhans Pal <paramhanspal65@gmail.com>
2025-11-27 07:26:38 +0530

    package and package.json uses and gitignore

---
+++ colors.js
@@ -0,0 +1,6 @@
+const colors = require("colors");
+
+console.log(colors.red("hello this is red color"));
+console.log(colors.green("hello this is red color"));
+console.log(colors.gray("hello this is red color"));
+console.log(colors.yellow("hello this is red color"));
--- createServer.js
+++ createServer.js
@@ -1,12 +1,12 @@
-// craete Server 
-// what is the work of req and response     req - getting value from frontend res - send the response from the server 
-// can we create multiple server in same file - yes 
- 
+// craete Server
+// what is the work of req and response     req - getting value from frontend res - send the response from the server
+// can we create multiple server in same file - yes
+//core package use to create server - http
+
 const http = require("http");
 http
   .createServer((req, res) => {
     res.write("<h1>Hello i am server<h1>");
-    res.end("end of response"); //this is nessessary to complete the response or end 
+    res.end("end of response"); //this is nessessary to complete the response or end
   })
   .listen(3300);
- 
